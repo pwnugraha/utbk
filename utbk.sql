e50ffbcf161e47eb938f345044789acd0cc8fd4c
Prima W N <primawn@gmail.com>
2020-11-30 20:14:35 +0700

    modify data structure

---
+++ utbk.sql
@@ -0,0 +1,449 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 30, 2020 at 02:11 PM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 7.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `utbk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bank_soal`
+--
+
+CREATE TABLE `bank_soal` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8mb4 NOT NULL,
+  `kategori_soal_id` int(10) UNSIGNED NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `butir_paket_soal`
+--
+
+CREATE TABLE `butir_paket_soal` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `paket_soal_id` int(10) UNSIGNED NOT NULL,
+  `soal_id` int(10) UNSIGNED NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups`
+--
+
+CREATE TABLE `groups` (
+  `id` mediumint(8) UNSIGNED NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `description` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `groups`
+--
+
+INSERT INTO `groups` (`id`, `name`, `description`) VALUES
+(1, 'admin', 'Administrator'),
+(2, 'members', 'General User');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori_soal`
+--
+
+CREATE TABLE `kategori_soal` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `category` varchar(10) CHARACTER SET utf8mb4 NOT NULL,
+  `subject` varchar(50) CHARACTER SET utf8mb4 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `kategori_soal`
+--
+
+INSERT INTO `kategori_soal` (`id`, `category`, `subject`) VALUES
+(1, 'saintek', 'matematika'),
+(2, 'saintek', 'biologi'),
+(3, 'saintek', 'fisika'),
+(4, 'saintek', 'kimia'),
+(5, 'soshum', 'geografi'),
+(6, 'soshum', 'ekonomi'),
+(7, 'soshum', 'sejarah'),
+(8, 'soshum', 'sosiologi'),
+(9, 'soshum', 'matematika soshum'),
+(10, 'tps', 'penalaran umum'),
+(11, 'tps', 'pemahaman bacaan dan menulis'),
+(12, 'tps', 'pengetahuan dan pemahaman umum'),
+(13, 'tps', 'pengetahuan kuantitatif');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_attempts`
+--
+
+CREATE TABLE `login_attempts` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `login` varchar(100) NOT NULL,
+  `time` int(11) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paket_soal`
+--
+
+CREATE TABLE `paket_soal` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8mb4 NOT NULL,
+  `description` text CHARACTER SET utf8mb4,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `soal`
+--
+
+CREATE TABLE `soal` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `bank_soal_id` int(10) UNSIGNED NOT NULL,
+  `kategori_soal_id` int(10) UNSIGNED NOT NULL,
+  `description` text CHARACTER SET utf8mb4 NOT NULL,
+  `opt1` text CHARACTER SET utf8mb4 NOT NULL,
+  `opt2` text CHARACTER SET utf8mb4 NOT NULL,
+  `opt3` text CHARACTER SET utf8mb4 NOT NULL,
+  `opt4` text CHARACTER SET utf8mb4 NOT NULL,
+  `opt5` text CHARACTER SET utf8mb4 NOT NULL,
+  `answer` tinyint(1) UNSIGNED NOT NULL,
+  `explanation` text CHARACTER SET utf8mb4 NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tryout`
+--
+
+CREATE TABLE `tryout` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8mb4 NOT NULL,
+  `description` text CHARACTER SET utf8mb4 NOT NULL,
+  `duration` varchar(3) CHARACTER SET utf8mb4 NOT NULL,
+  `start_date` datetime NOT NULL,
+  `end_date` datetime NOT NULL,
+  `status` tinyint(1) UNSIGNED NOT NULL,
+  `type` tinyint(1) UNSIGNED NOT NULL,
+  `paket_soal_id` int(10) UNSIGNED NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tryout_schedule`
+--
+
+CREATE TABLE `tryout_schedule` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `tryout_id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8mb4 NOT NULL,
+  `description` text CHARACTER SET utf8mb4,
+  `quota` int(11) NOT NULL,
+  `start_date` datetime NOT NULL,
+  `end_date` datetime NOT NULL,
+  `status` tinyint(1) UNSIGNED NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(254) NOT NULL,
+  `activation_selector` varchar(255) DEFAULT NULL,
+  `activation_code` varchar(255) DEFAULT NULL,
+  `forgotten_password_selector` varchar(255) DEFAULT NULL,
+  `forgotten_password_code` varchar(255) DEFAULT NULL,
+  `forgotten_password_time` int(11) UNSIGNED DEFAULT NULL,
+  `remember_selector` varchar(255) DEFAULT NULL,
+  `remember_code` varchar(255) DEFAULT NULL,
+  `created_on` int(11) UNSIGNED NOT NULL,
+  `last_login` int(11) UNSIGNED DEFAULT NULL,
+  `active` tinyint(1) UNSIGNED DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `company` varchar(100) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `email`, `activation_selector`, `activation_code`, `forgotten_password_selector`, `forgotten_password_code`, `forgotten_password_time`, `remember_selector`, `remember_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `company`, `phone`) VALUES
+(1, '127.0.0.1', 'administrator', '$2y$12$0LCh5QeULgGcZRIeuzDfzOjNpf7SGmdUuEyNbKiT7uoqTYicTWHyG', 'admin@admin.com', NULL, '', NULL, NULL, NULL, NULL, NULL, 1268889823, 1606717671, 1, 'Admin', 'istrator', 'ADMIN', '0'),
+(6, '::1', 'dawd', '$2y$10$dHpI8BUcjmaC3jcJd7N8wuGfDoVOvSpUgaj790k/XHIeCXZ421QBO', 'primawnugraha@gmail.com', 'd1257097f27cab335011', '$2y$10$PVLrNqZwhmuT6d2xrcq7v.lN6j7rY7eAM65XdQR1TYkRi4uYhUdBa', NULL, NULL, NULL, NULL, NULL, 1606718358, NULL, 0, 'Prima', 'Nugraha', 'adawdwa', '081901026006');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_groups`
+--
+
+CREATE TABLE `users_groups` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `group_id` mediumint(8) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users_groups`
+--
+
+INSERT INTO `users_groups` (`id`, `user_id`, `group_id`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(7, 6, 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bank_soal`
+--
+ALTER TABLE `bank_soal`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_bank_soal_kategori_soal` (`kategori_soal_id`);
+
+--
+-- Indexes for table `butir_paket_soal`
+--
+ALTER TABLE `butir_paket_soal`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_butir_paket_soal_paket_soal` (`paket_soal_id`),
+  ADD KEY `fk_butir_paket_soal_soal` (`soal_id`);
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kategori_soal`
+--
+ALTER TABLE `kategori_soal`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `login_attempts`
+--
+ALTER TABLE `login_attempts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `paket_soal`
+--
+ALTER TABLE `paket_soal`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `soal`
+--
+ALTER TABLE `soal`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `bank_soal_id` (`bank_soal_id`) USING BTREE,
+  ADD KEY `fk_soal_kategori_soal` (`kategori_soal_id`);
+
+--
+-- Indexes for table `tryout`
+--
+ALTER TABLE `tryout`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_tryout_paket_soal` (`paket_soal_id`);
+
+--
+-- Indexes for table `tryout_schedule`
+--
+ALTER TABLE `tryout_schedule`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_tryout_schedule_tryout` (`tryout_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uc_email` (`email`),
+  ADD UNIQUE KEY `uc_activation_selector` (`activation_selector`),
+  ADD UNIQUE KEY `uc_forgotten_password_selector` (`forgotten_password_selector`),
+  ADD UNIQUE KEY `uc_remember_selector` (`remember_selector`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `users_groups`
+--
+ALTER TABLE `users_groups`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uc_users_groups` (`user_id`,`group_id`),
+  ADD KEY `fk_users_groups_users1_idx` (`user_id`),
+  ADD KEY `fk_users_groups_groups1_idx` (`group_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bank_soal`
+--
+ALTER TABLE `bank_soal`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `butir_paket_soal`
+--
+ALTER TABLE `butir_paket_soal`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `kategori_soal`
+--
+ALTER TABLE `kategori_soal`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `login_attempts`
+--
+ALTER TABLE `login_attempts`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `paket_soal`
+--
+ALTER TABLE `paket_soal`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `soal`
+--
+ALTER TABLE `soal`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tryout`
+--
+ALTER TABLE `tryout`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tryout_schedule`
+--
+ALTER TABLE `tryout_schedule`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `users_groups`
+--
+ALTER TABLE `users_groups`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bank_soal`
+--
+ALTER TABLE `bank_soal`
+  ADD CONSTRAINT `fk_bank_soal_kategori_soal` FOREIGN KEY (`kategori_soal_id`) REFERENCES `kategori_soal` (`id`);
+
+--
+-- Constraints for table `butir_paket_soal`
+--
+ALTER TABLE `butir_paket_soal`
+  ADD CONSTRAINT `fk_butir_paket_soal_paket_soal` FOREIGN KEY (`paket_soal_id`) REFERENCES `paket_soal` (`id`),
+  ADD CONSTRAINT `fk_butir_paket_soal_soal` FOREIGN KEY (`soal_id`) REFERENCES `soal` (`id`);
+
+--
+-- Constraints for table `soal`
+--
+ALTER TABLE `soal`
+  ADD CONSTRAINT `fk_soal_bank_soal` FOREIGN KEY (`bank_soal_id`) REFERENCES `bank_soal` (`id`),
+  ADD CONSTRAINT `fk_soal_kategori_soal` FOREIGN KEY (`kategori_soal_id`) REFERENCES `kategori_soal` (`id`);
+
+--
+-- Constraints for table `tryout`
+--
+ALTER TABLE `tryout`
+  ADD CONSTRAINT `fk_tryout_paket_soal` FOREIGN KEY (`paket_soal_id`) REFERENCES `paket_soal` (`id`);
+
+--
+-- Constraints for table `tryout_schedule`
+--
+ALTER TABLE `tryout_schedule`
+  ADD CONSTRAINT `fk_tryout_schedule_tryout` FOREIGN KEY (`tryout_id`) REFERENCES `tryout` (`id`);
+
+--
+-- Constraints for table `users_groups`
+--
+ALTER TABLE `users_groups`
+  ADD CONSTRAINT `fk_users_groups_groups1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_users_groups_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
