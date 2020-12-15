3b473ad56c70a9af0ae9859dec16498cb3975f01
Jody-septiawan <jody.septiawan5@gmail.com>
2020-12-15 21:32:04 +0700

    add bootstrap

--- application/views/exam/start-template/header.php
+++ application/views/exam/start-template/header.php
@@ -12,10 +12,13 @@
     <link rel="stylesheet" href="<?= base_url('asset/exam/css/style.css') ?>">
 
     <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?= base_url('asset/master/bootstrap-4.5.3/dist/css/bootstrap.min.css')  ?>">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="<?= base_url('asset/exam/js/jquery-3.5.1.min.js') ?>"></script>
     <script src="<?= base_url('asset/exam/js/jQuery.countdownTimer.js') ?>"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <script src="<?= base_url('asset/master/bootstrap-4.5.3/dist/js/bootstrap.bundle.min.js') ?>"></script>
 
     <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.countdown/2.2.0/jquery.countdown.min.js" integrity="sha512-lteuRD+aUENrZPTXWFRPTBcDDxIGWe5uu0apPEn+3ZKYDwDaEErIK9rvR0QzUGmUQ55KFE2RqGTVoZsKctGMVw==" crossorigin="anonymous"></script> -->
 
